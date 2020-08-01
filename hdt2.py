b791b1ba63dc53c9bc93b4dded9e2e089eecf9ea
Rodrigo Zea <zearodrigo37@gmail.com>
2020-08-01 16:41:58 -0600

    starting hamming code

--- hdt2.py
+++ hdt2.py
@@ -15,6 +15,18 @@ ba = bitarray.bitarray()
 userTxt = input("Ingrese un mensaje a enviar: ")
 # Use bitarray to convert
 ba.frombytes(userTxt.encode('utf-8'))
+print(ba)
+print(ba[0])
+
 # Hamming Code
+m = len(ba)
+
+# Calculate redundant bits
+# Use the formula: 2^r >= m + r + 1
+for i in range(m):
+    if (2**i >= m + i + 1):
+        return i
+
+
 # Add noise
 # Send to socket
